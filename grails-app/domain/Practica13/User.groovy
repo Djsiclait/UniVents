be9070cfb562cb8ef7a41ec899de2c28df167afa
Djsiclait <djidjellypas92@gmail.com>
2016-11-21 19:45:48 -0400

    Create User groovy

---
+++ grails-app/domain/Practica13/User.groovy
@@ -0,0 +1,12 @@
+package Practica13
+
+class User implements Serializable {
+
+    // Attributes
+    String username
+    String firstName
+    String lastName
+
+    static constraints = {
+    }
+}
