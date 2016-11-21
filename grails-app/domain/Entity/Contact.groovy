08b40d7fa2efcd6f6c3c24ce21d9852a9b82d322
Djsiclait <djidjellypas92@gmail.com>
2016-11-21 19:42:01 -0400

    Create contact groovy

---
+++ grails-app/domain/Entity/Contact.groovy
@@ -0,0 +1,16 @@
+package Entity
+
+class Contact {
+
+    // Attributes
+    String firstName
+    String lastName
+    String email
+    String telephone
+    String address
+    String job
+    String cellphone
+
+    static constraints = {
+    }
+}
